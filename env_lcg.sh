dbc6665c6164457f16849859feaaeb93aefcf889
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2023-12-19 18:42:51 -0600

    Update to Coffea 0.7.22
    Updating Coffea to the latest version before Coffea 2023. Coffea 0.7.22
    requires Python 3.7-3.11, so the LCG packages sourced by env_lcg.sh
    needed to to be updated accordingly. The script now loads Python 3.9
    from
    /cvmfs/sft.cern.ch/lcg/views/LCG_104c/x86_64-centos7-gcc11-opt/setup.sh.
    This means that the previous setup.sh file for SL6 is no longer valid
    and was removed.

--- env_lcg.sh
+++ env_lcg.sh
@@ -1,12 +1,12 @@
-# http://lcginfo.cern.ch/release/95apython3/
-# Try to guess SL6 vs. CC7
-if uname -r | grep -q el6; then
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-slc6-gcc8-opt/setup.sh
+# https://lcginfo.cern.ch/release_packages/104c/x86_64-centos7-gcc11-opt/
+# Configured for Python 3.9 on CC7
+if uname -r | grep -q el7; then
+  source /cvmfs/sft.cern.ch/lcg/views/LCG_104c/x86_64-centos7-gcc11-opt/setup.sh
 else
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
+  echo "Cannot source LCG setup.sh file. Only configured for Python 3.9 on CC7"
 fi
 
-export PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
+export PYTHONPATH=~/.local/lib/python3.9/site-packages:$PYTHONPATH
 export PYTHONPATH=$PWD/analysis:$PYTHONPATH
 export PYTHONWARNINGS="ignore"
 #export PATH=~/.local/bin:$PATH
--- setup_lcg.sh
+++ setup_lcg.sh
@@ -2,7 +2,7 @@
 
 source env_lcg.sh
 
-pip install --user coffea==0.7.20
+pip install --user coffea==0.7.22
 pip install --user https://github.com/mcremone/rhalphalib/archive/py3.zip
 pip install --user xxhash
 # progressbar, sliders, etc.
