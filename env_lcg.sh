dcda66048d1d87c805d86a112726b1febb0463d7
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2023-12-21 00:19:23 -0600

    Migrate to Python 3.8 in CMSSW_11_3_4
    Move to the python3 version that comes with CMSSW_11_3_4, which is the
    current recommended version for the combine tool. The decaf repository
    will now be run inside CMSSW_11_3_4/src. See the discussion in Issue #83
    for more details.
    
    The rhalphalib package installation is also updated to install the
    official repo.

--- env_lcg.sh
+++ env_lcg.sh
@@ -1,12 +1,4 @@
-# https://lcginfo.cern.ch/release_packages/104c/x86_64-centos7-gcc11-opt/
-# Configured for Python 3.9 on CC7
-if uname -r | grep -q el7; then
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_104c/x86_64-centos7-gcc11-opt/setup.sh
-else
-  echo "Cannot source LCG setup.sh file. Only configured for Python 3.9 on CC7"
-fi
-
-export PYTHONPATH=~/.local/lib/python3.9/site-packages:$PYTHONPATH
+export PYTHONPATH=~/.local/lib/python3.8/site-packages:$PYTHONPATH
 export PYTHONPATH=$PWD/analysis:$PYTHONPATH
 export PYTHONWARNINGS="ignore"
 #export PATH=~/.local/bin:$PATH
--- setup_lcg.sh
+++ setup_lcg.sh
@@ -2,9 +2,9 @@
 
 source env_lcg.sh
 
-pip install --user coffea==0.7.22
-pip install --user https://github.com/mcremone/rhalphalib/archive/py3.zip
-pip install --user xxhash
+python3 -m pip install --user coffea==0.7.22
+python3 -m pip install --user https://github.com/nsmith-/rhalphalib/archive/master.zip
+python3 -m pip install --user xxhash
 # progressbar, sliders, etc.
 jupyter nbextension enable --py widgetsnbextension
 
