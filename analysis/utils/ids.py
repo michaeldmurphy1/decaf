f0b748e5b4533c63423903cbf174eceee0b4b9fc
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2023-12-20 20:25:26 -0600

    Fix formatting issues in ids.py
    When reformatting ids.py to be more legible previously, the parentheses
    that were removed inbetween conditional statements with comparisons
    cannot be removed to preserve the intended behavior.
    
    In Python, the bitwise operators & and | are executed before comparison
    operators like <, ==, etc., which is not the intended behavior. On the
    other hand, the boolean operators "and" and "or" are executed after
    comparison operators as intended, but these operators are not overloaded
    for this context. (See "Operator Precedence" in the offical Python
    documentation).
    
    Bitwise NOT ~ does come before & and |, and therefore does not require
    the additional parentheses in these functions.
    
    Some addtional formatting tweaks were also made to ids.py.

--- analysis/utils/ids.py
+++ analysis/utils/ids.py
@@ -1,13 +1,14 @@
 import awkward
 import uproot
-#import uproot_methods
 import numpy as np
 from coffea.util import save
+
 
 def Mask(pt):
     # Just a complicated way to initialize a jagged array with the needed shape
     # to True
-    return ~(pt==np.nan)
+    return ~(pt == np.nan)
+
 
 ######
 ## Electron
@@ -16,74 +17,107 @@ def Mask(pt):
 ## https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2
 ######
 
+
 def isLooseElectron(pt, eta, dxy, dz, loose_id, year):
     mask = Mask(pt)
-    if year=='2016':
+    if year == "2016":
         mask = (
-            pt>10 &
-            abs(eta)<1.4442 &
-            abs(dxy)<0.05 & abs(dz)<0.1 & loose_id>=2
+            (pt > 10)
+            & (abs(eta) < 1.4442)
+            & (abs(dxy) < 0.05)
+            & (abs(dz) < 0.1)
+            & (loose_id >= 2)
         ) | (
-            pt>10 &
-            abs(eta)>1.5660 & abs(eta)<2.5 &
-            abs(dxy)<0.1 & abs(dz)<0.2 & loose_id>=2
+            (pt > 10)
+            & (abs(eta) > 1.5660)
+            & (abs(eta) < 2.5)
+            & (abs(dxy) < 0.1)
+            & (abs(dz) < 0.2)
+            & (loose_id >= 2)
         )
-    elif year=='2017':
+    elif year == "2017":
         mask = (
-            pt>10 &
-            abs(eta)<1.4442 &
-            abs(dxy)<0.05 & abs(dz)<0.1 & loose_id>=2
+            (pt > 10)
+            & (abs(eta) < 1.4442)
+            & (abs(dxy) < 0.05)
+            & (abs(dz) < 0.1)
+            & (loose_id >= 2)
         ) | (
-            pt>10 &
-            abs(eta)>1.5660 & abs(eta)<2.5 &
-            abs(dxy)<0.1 & abs(dz)<0.2 & loose_id>=2
+            (pt > 10)
+            & (abs(eta) > 1.5660)
+            & (abs(eta) < 2.5)
+            & (abs(dxy) < 0.1)
+            & (abs(dz) < 0.2)
+            & (loose_id >= 2)
         )
-    elif year=='2018':
+    elif year == "2018":
         mask = (
-            pt>10 &
-            abs(eta)<1.4442 &
-            abs(dxy)<0.05 & abs(dz)<0.1 & loose_id>=2
+            (pt > 10)
+            & (abs(eta) < 1.4442)
+            & (abs(dxy) < 0.05)
+            & (abs(dz) < 0.1)
+            & (loose_id >= 2)
         ) | (
-            pt>10 &
-            abs(eta)>1.5660 & abs(eta)<2.5 &
-            abs(dxy)<0.1 & abs(dz)<0.2 & loose_id>=2
+            (pt > 10)
+            & (abs(eta) > 1.5660)
+            & (abs(eta) < 2.5)
+            & (abs(dxy) < 0.1)
+            & (abs(dz) < 0.2)
+            & (loose_id >= 2)
         )
     return mask
+
 
 def isTightElectron(pt, eta, dxy, dz, tight_id, year):
     # 2017/18 pT thresholds adjusted to match monojet, using dedicated ID SFs
     mask = Mask(pt)
-    if year=='2016': # Trigger: HLT_Ele27_WPTight_Gsf_v
+    if year == "2016":  # Trigger: HLT_Ele27_WPTight_Gsf_v
         mask = (
-            pt>40 &
-            abs(eta)<1.4442 &
-            abs(dxy)<0.05 & abs(dz)<0.1 & tight_id==4
+            (pt > 40)
+            & (abs(eta) < 1.4442)
+            & (abs(dxy) < 0.05)
+            & (abs(dz) < 0.1)
+            & (tight_id == 4)
         ) | (
-            pt>40 &
-            abs(eta)>1.5660 & abs(eta)<2.5 &
-            abs(dxy)<0.1 & abs(dz)<0.2 & tight_id==4
+            (pt > 40)
+            & (abs(eta) > 1.5660)
+            & (abs(eta) < 2.5)
+            & (abs(dxy) < 0.1)
+            & (abs(dz) < 0.2)
+            & (tight_id == 4)
         )
-    elif year=='2017': # Trigger: HLT_Ele35_WPTight_Gsf_v
+    elif year == "2017":  # Trigger: HLT_Ele35_WPTight_Gsf_v
         mask = (
-            pt>40 &
-            abs(eta)<1.4442 &
-            abs(dxy)<0.05 & abs(dz)<0.1 & tight_id==4
+            (pt > 40)
+            & (abs(eta) < 1.4442)
+            & (abs(dxy) < 0.05)
+            & (abs(dz) < 0.1)
+            & (tight_id == 4)
         ) | (
-            pt>40 &
-            abs(eta)>1.5660 & abs(eta)<2.5 &
-            abs(dxy)<0.1 & abs(dz)<0.2 & tight_id==4
+            (pt > 40)
+            & (abs(eta) > 1.5660)
+            & (abs(eta) < 2.5)
+            & (abs(dxy) < 0.1)
+            & (abs(dz) < 0.2)
+            & (tight_id == 4)
         )
-    elif year=='2018': # Trigger: HLT_Ele32_WPTight_Gsf_v
+    elif year == "2018":  # Trigger: HLT_Ele32_WPTight_Gsf_v
         mask = (
-            pt>40 &
-            abs(eta)<1.4442 &
-            abs(dxy)<0.05 & abs(dz)<0.1 & tight_id==4
+            (pt > 40)
+            & (abs(eta) < 1.4442)
+            & (abs(dxy) < 0.05)
+            & (abs(dz) < 0.1)
+            & (tight_id == 4)
         ) | (
-            pt>40 &
-            abs(eta)>1.5660 & abs(eta)<2.5 &
-            abs(dxy)<0.1 & abs(dz)<0.2 & tight_id==4
+            (pt > 40)
+            & (abs(eta) > 1.5660)
+            & (abs(eta) < 2.5)
+            & (abs(dxy) < 0.1)
+            & (abs(dz) < 0.2)
+            & (tight_id == 4)
         )
     return mask
+
 
 #######
 ## Muon
@@ -93,74 +127,42 @@ def isTightElectron(pt, eta, dxy, dz, tight_id, year):
 ## https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMuonSelection#Muon_Isolation
 #######
 
+
 def isLooseMuon(pt, eta, iso, loose_id, year):
     # dxy and dz cuts are missing from med_id; loose isolation is 0.25
     mask = Mask(pt)
-    if year=='2016':
-        mask = (
-            pt>20 &
-            abs(eta)<2.4 &
-            loose_id & iso<0.25
-        )
-    elif year=='2017':
-        mask = (
-            pt>20 &
-            abs(eta)<2.4 &
-            loose_id & iso<0.25
-        )
-    elif year=='2018':
-        mask = (
-            pt>15 &
-            abs(eta)<2.4 &
-            loose_id & iso<0.25
-        )
+    if year == "2016":
+        mask = (pt > 20) & (abs(eta) < 2.4) & loose_id & (iso < 0.25)
+    elif year == "2017":
+        mask = (pt > 20) & (abs(eta) < 2.4) & loose_id & (iso < 0.25)
+    elif year == "2018":
+        mask = (pt > 15) & (abs(eta) < 2.4) & loose_id & (iso < 0.25)
     return mask
+
 
 def isTightMuon(pt, eta, iso, tight_id, year):
     # dxy and dz cuts are baked on tight_id; tight isolation is 0.15
     mask = Mask(pt)
-    if year=='2016':
-        mask = (
-            pt>30 &
-            abs(eta)<2.4 &
-            tight_id & iso<0.15
-        )
-    elif year=='2017':
-        mask = (
-            pt>30 &
-            abs(eta)<2.4 &
-            tight_id & iso<0.15
-        )
-    elif year=='2018':
-        mask = (
-            pt>30 &
-            abs(eta)<2.4 &
-            tight_id & iso<0.15
-        )
+    if year == "2016":
+        mask = (pt > 30) & (abs(eta) < 2.4) & tight_id & (iso < 0.15)
+    elif year == "2017":
+        mask = (pt > 30) & (abs(eta) < 2.4) & tight_id & (iso < 0.15)
+    elif year == "2018":
+        mask = (pt > 30) & (abs(eta) < 2.4) & tight_id & (iso < 0.15)
     return mask
+
 
 def isSoftMuon(pt, eta, iso, tight_id, year):
     # dxy and dz cuts are baked on tight_id; tight isolation is 0.15
     mask = Mask(pt)
-    if year=='2016':
-        mask = (
-            pt>5 &
-            abs(eta)<2.4 &
-            tight_id & iso>0.15
-        )
-    elif year=='2017':
-        mask = (
-            pt>5 &
-            abs(eta)<2.4 &
-            tight_id & iso>0.15
-        )
-    elif year=='2018':
-        mask = (
-            pt>5 &
-            abs(eta)<2.4 &
-            tight_id & iso>0.15
-        )
+    if year == "2016":
+        mask = (pt > 5) & (abs(eta) < 2.4) & tight_id & (iso > 0.15)
+    elif year == "2017":
+        mask = (pt > 5) & (abs(eta) < 2.4) & tight_id & (iso > 0.15)
+    elif year == "2018":
+        mask = (pt > 5) & (abs(eta) < 2.4) & tight_id & (iso > 0.15)
     return mask
+
 
 ######
 ## Tau
@@ -180,30 +182,44 @@ def isSoftMuon(pt, eta, iso, tight_id, year):
 ## 1 = VLoose, 2 = Loose, 4 = Medium, 8 = Tight
 ######
 
+
 def isLooseTau(pt, eta, decayMode, decayModeDMs, ide, idj, idm, year):
     mask = Mask(pt)
-    if year=='2016':
+    if year == "2016":
         mask = (
-            pt>20 &
-            abs(eta)<2.3 &
-            ~(decayMode==5) & ~(decayMode==6) & decayModeDMs &
-            (ide&16)==16 & (idj&4)==4 & (idm&2)==2
+            (pt > 20)
+            & (abs(eta) < 2.3)
+            & ~(decayMode == 5)
+            & ~(decayMode == 6)
+            & decayModeDMs
+            & ((ide & 16) == 16)
+            & ((idj & 4) == 4)
+            & ((idm & 2) == 2)
         )
-    elif year=='2017':
+    elif year == "2017":
         mask = (
-            pt>20 &
-            abs(eta)<2.3 &
-            ~(decayMode==5) & ~(decayMode==6) & decayModeDMs &
-            (ide&16)==16 & (idj&4)==4 & (idm&2)==2
+            (pt > 20)
+            & (abs(eta) < 2.3)
+            & ~(decayMode == 5)
+            & ~(decayMode == 6)
+            & decayModeDMs
+            & ((ide & 16) == 16)
+            & ((idj & 4) == 4)
+            & ((idm & 2) == 2)
         )
-    elif year=='2018':
+    elif year == "2018":
         mask = (
-            pt>20 &
-            abs(eta)<2.3 &
-            ~(decayMode==5) & ~(decayMode==6) & decayModeDMs &
-            (ide&16)==16 & (idj&4)==4 & (idm&2)==2
+            (pt > 20)
+            & (abs(eta) < 2.3)
+            & ~(decayMode == 5)
+            & ~(decayMode == 6)
+            & decayModeDMs
+            & ((ide & 16) == 16)
+            & ((idj & 4) == 4)
+            & ((idm & 2) == 2)
         )
     return mask
+
 
 ######
 ## Photon
@@ -213,49 +229,49 @@ def isLooseTau(pt, eta, decayMode, decayModeDMs, ide, idj, idm, year):
 ## Note: Photon IDs are integers, not bit masks
 ######
 
+
 def isLoosePhoton(pt, eta, loose_id, year):
     mask = Mask(pt)
-    if year=='2016':
+    if year == "2016":
         mask = (
-            pt>20 &
-            ~(abs(eta)>1.4442) & abs(eta)<1.5660 & abs(eta)<2.5 &
-            loose_id>=1
+            (pt > 20)
+            & ~(abs(eta) > 1.4442)
+            & (abs(eta) < 1.5660)
+            & (abs(eta) < 2.5)
+            & (loose_id >= 1)
         )
-    elif year=='2017':
+    elif year == "2017":
         mask = (
-            pt>20 &
-            ~(abs(eta)>1.4442) & abs(eta)<1.5660 & abs(eta)<2.5 &
-            loose_id>=1
+            (pt > 20)
+            & ~(abs(eta) > 1.4442)
+            & (abs(eta) < 1.5660)
+            & (abs(eta) < 2.5)
+            & (loose_id >= 1)
         )
-    elif year=='2018':
+    elif year == "2018":
         mask = (
-            pt>20 &
-            ~(abs(eta)>1.4442) & abs(eta)<1.5660 & abs(eta)<2.5 &
-            loose_id>=1
+            (pt > 20)
+            & ~(abs(eta) > 1.4442)
+            & (abs(eta) < 1.5660)
+            & (abs(eta) < 2.5)
+            & (loose_id >= 1)
         )
     return mask
 
+
 def isTightPhoton(pt, tight_id, year):
-    #isScEtaEB is used (barrel only), so no eta requirement
-    #2017/18 pT requirement adjusted to match monojet, using dedicated ID SFs
-    #Tight photon use medium ID, as in monojet
+    # isScEtaEB is used (barrel only), so no eta requirement
+    # 2017/18 pT requirement adjusted to match monojet, using dedicated ID SFs
+    # Tight photon use medium ID, as in monojet
     mask = Mask(pt)
-    if year=='2016':
-        mask = (
-            pt>200 &
-            tight_id>=2
-        )
-    elif year=='2017':
-        mask = (
-            pt>230 &
-            (tight_id&2)==2
-        )
-    elif year=='2018':
-        mask = (
-            pt>230 &
-            (tight_id&2)==2
-        )
+    if year == "2016":
+        mask = (pt > 200) & (tight_id >= 2)
+    elif year == "2017":
+        mask = (pt > 230) & ((tight_id & 2) == 2)
+    elif year == "2018":
+        mask = (pt > 230) & ((tight_id & 2) == 2)
     return mask
+
 
 ######
 ## Fatjet
@@ -263,13 +279,13 @@ def isTightPhoton(pt, tight_id, year):
 ## Tight working point including lepton veto (TightLepVeto)
 ######
 
+
 def isGoodFatJet(pt, eta, jet_id, nhf, chf):
     mask = (
-        pt>160 &
-        abs(eta)<2.4 &
-        (jet_id&6)==6 & nhf<0.8 & chf>0.1
+        (pt > 160) & (abs(eta) < 2.4) & ((jet_id & 6) == 6) & (nhf < 0.8) & (chf > 0.1)
     )
     return mask
+
 
 ######
 ## Jet
@@ -294,43 +310,38 @@ def isGoodFatJet(pt, eta, jet_id, nhf, chf):
 ## Jet_puId = (passlooseID*4 + passmediumID*2 + passtightID*1).
 ######
 
+
 def isGoodJet(pt, eta, jet_id, pu_id, year):
-    mask = (
-        pt>30 &
-        abs(eta)<2.4 &
-        (jet_id&6)==6
-    )
-    if year=='2016':
-        mask = ((pt>=50) & mask) | ((pt<50) & mask & (pu_id&1)==1)
-    elif year=='2017':
-        mask = ((pt>=50) & mask) | ((pt<50) & mask & (pu_id&4)==4)
-    elif year=='2018':
-        mask = ((pt>=50) & mask) | ((pt<50) & mask & (pu_id&4)==4)
+    mask = (pt > 30) & (abs(eta) < 2.4) & ((jet_id & 6) == 6)
+    if year == "2016":
+        mask = ((pt >= 50) & mask) | ((pt < 50) & mask & ((pu_id & 1) == 1))
+    elif year == "2017":
+        mask = ((pt >= 50) & mask) | ((pt < 50) & mask & ((pu_id & 4) == 4))
+    elif year == "2018":
+        mask = ((pt >= 50) & mask) | ((pt < 50) & mask & ((pu_id & 4) == 4))
     return mask
+
 
 ######
 ## HEM
 ######
 
+
 def isHEMJet(pt, eta, phi):
-    mask = (
-        pt>30 &
-        eta>-3.0 & eta<-1.3 &
-        phi>-1.57 & phi<-0.87
-    )
+    mask = (pt > 30) & (eta > -3.0) & (eta < -1.3) & (phi > -1.57) & (phi < -0.87)
     return mask
 
 
 ids = {}
-ids['isLooseElectron'] = isLooseElectron
-ids['isTightElectron'] = isTightElectron
-ids['isLooseMuon']     = isLooseMuon
-ids['isTightMuon']     = isTightMuon
-ids['isSoftMuon']     = isSoftMuon
-ids['isLooseTau']      = isLooseTau
-ids['isLoosePhoton']   = isLoosePhoton
-ids['isTightPhoton']   = isTightPhoton
-ids['isGoodJet']       = isGoodJet
-ids['isGoodFatJet']    = isGoodFatJet
-ids['isHEMJet']        = isHEMJet
-save(ids, 'data/test_ids.coffea')
+ids["isLooseElectron"] = isLooseElectron
+ids["isTightElectron"] = isTightElectron
+ids["isLooseMuon"] = isLooseMuon
+ids["isTightMuon"] = isTightMuon
+ids["isSoftMuon"] = isSoftMuon
+ids["isLooseTau"] = isLooseTau
+ids["isLoosePhoton"] = isLoosePhoton
+ids["isTightPhoton"] = isTightPhoton
+ids["isGoodJet"] = isGoodJet
+ids["isGoodFatJet"] = isGoodFatJet
+ids["isHEMJet"] = isHEMJet
+save(ids, "data/test_ids.coffea")
