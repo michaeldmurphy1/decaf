ebb89e9c07daadbe91b014cace40f33929d6ae57
Sena Durgut <sena@senas-mbp.wifi.local.cmu.edu>
2024-02-07 11:55:46 -0500

    updated list.py for UL

--- analysis/macros/list.py
+++ analysis/macros/list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 from optparse import OptionParser
+import subprocess
+import json 
 
 parser = OptionParser()
 parser.add_option('-y', '--year', help='year', dest='year')
@@ -9,33 +11,51 @@ parser.add_option('-y', '--year', help='year', dest='year')
 globalredirect = "root://xrootd-cms.infn.it/"
 
 campaigns ={}
-campaigns['2016'] = 'RunIISummer16MiniAODv3'
-campaigns['2017'] = 'RunIIFall17MiniAODv2'
-campaigns['2018'] = 'RunIIAutumn18NanoAODv6'
+campaigns['2016'] = 'UL2016'
+campaigns['2017'] = 'UL2017'
+campaigns['2018'] = 'UL2018'
 
-slist = ['DarkHiggs_MonoHs_LO_TuneCP5_13TeV-madgraph-pythia8']
-datasets = []
-for sample in slist:
-    query="dasgoclient --query=\"dataset dataset=/"+sample+"*/"+campaigns[options.year]+"*/NANOAODSIM\""
-    print(query)
-    os.system(query+" >"+sample+".txt")
-    datasets.append(sample+".txt")
 
-filenames = []
-for dname in datasets:                                                                                                                                                                 
-    with open(dname) as infile:
-        for line in infile:
-            dataset = line.strip()
-            query="dasgoclient --query=\"file dataset="+dataset+"\""
-            print(query)
-            os.system(query+" > "+dataset.split('/')[1]+"____"+dataset.split('/')[2]+".txt")
-            filenames.append(dataset.split('/')[1]+"____"+dataset.split('/')[2]+".txt")
-    os.system('rm '+dname)
+slist = ['SingleMuon']
+
+if options.year == '2016' or options.year == '2017':
+    slist += ['SingleElectron']
+if options.year == '2018':
+    slist += ['EGamma']
 
 for sample in slist:
-    with open('metadata/'+sample+'.txt', 'w') as outfile:
-        for fname in filenames:
-            with open(fname) as infile:
-                for line in infile:
-                    outfile.write(globalredirect+line)
-            os.system('rm '+fname)
+    # Initialize an empty dictionary to hold dataset information for the current sample
+    sample_data_structure = {}
+
+    # Construct the query to get datasets
+    dataset_query = f"dasgoclient --query=\"dataset=/{sample}/*{campaigns['2016']}*JMENano*/*\""
+    print('Querying datasets:', dataset_query)
+    
+    # Execute the query and decode the output
+    datasets = subprocess.check_output(dataset_query, shell=True).decode('utf-8').strip().split('\n')
+    
+    # Loop over each dataset to get the list of files
+    for dataset in datasets:
+        # Make sure dataset is not empty
+        if not dataset: continue
+        
+        # Construct the query to get files for the dataset
+        file_query = f"dasgoclient --query=\"file dataset={dataset}\""
+        print('Querying files in dataset:', file_query)
+        
+        # Execute the query and decode the output
+        files = subprocess.check_output(file_query, shell=True).decode('utf-8').strip().split('\n')
+        files = [globalredirect + file for file in files if file]  # Prepend redirect and filter out empty lines
+        
+        # Add the dataset and its files to the data structure
+        sample_data_structure[dataset] = {
+            "files": files
+        }
+
+    # Write the sample's data structure to a JSON file
+    sample_json_filename = f'metadata/{sample}.json'
+    with open(sample_json_filename, 'w') as outfile:
+        json.dump(sample_data_structure, outfile, indent=4)
+
+    print(f"Metadata for {sample} written to {sample_json_filename}")
+
