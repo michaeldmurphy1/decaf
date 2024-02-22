ddc8a9c753fe2faef42dc8101c9e291f5de42088
Matteo Cremonesi <matteoc@fnal.gov>
2024-02-21 23:49:55 -0500

    Update run.sh

--- analysis/run.sh
+++ analysis/run.sh
@@ -18,32 +18,29 @@ if [ "${4}" == "kisti" ]; then
         ls -l /tmp/x509up_u$(id -u)
         /usr/bin/voms-proxy-info -all
     fi
-    xrdcp -s root://cms-xrdr.private.lo:2094//xrd/store/user/$USER/decaf.tgz .
+    xrdcp -s root://cms-xrdr.private.lo:2094//xrd/store/user/$USER/cmssw_11_3_4.tgz .
     echo "Decaf correctly copied"
-    xrdcp -s root://cms-xrdr.private.lo:2094//xrd/store/user/$USER/pylocal.tgz .
+    xrdcp -s root://cms-xrdr.private.lo:2094//xrd/store/user/$USER/pylocal_3_8.tgz .
     echo "Python correctly copied"
 else
-    xrdcp -s root://cmseos.fnal.gov//store/user/$USER/decaf.tgz .
+    xrdcp -s root://cmseos.fnal.gov//store/user/$USER/cmssw_11_3_4.tgz .
     echo "Decaf correctly copied"
-    xrdcp -s root://cmseos.fnal.gov//store/user/$USER/pylocal.tgz .
+    xrdcp -s root://cmseos.fnal.gov//store/user/$USER/pylocal_3_8.tgz .
     echo "Python correctly copied"
 fi
-tar -zxvf decaf.tgz
-tar -zxvf pylocal.tgz
-rm decaf.tgz
-rm pylocal.tgz
-cd decaf
-if uname -r | grep -q el6; then
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-slc6-gcc8-opt/setup.sh
-else
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
-fi
+tar -zxvf cmssw_11_3_4.tgz
+tar -zxvf pylocal_3_8.tgz
+rm cmssw_11_3_4.tgz
+rm pylocal_3_8.tgz
 export PYTHONPATH=${_CONDOR_SCRATCH_DIR}/site-packages:$PYTHONPATH
 export PYTHONPATH=$(find ${_CONDOR_SCRATCH_DIR}/site-packages/ -name *.egg |tr '\n' ':')$PYTHONPATH
 export PYTHONWARNINGS="ignore"
 echo "Updated python path: " $PYTHONPATH
-cd analysis
-echo "python run.py --metadata ${1} --dataset ${2} --processor ${3}"
+cd CMSSW_11_3_4/src
+scramv1 b ProjectRename
+eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
+cd decaf/analysis
+echo "python3 run.py --metadata ${1} --dataset ${2} --processor ${3}"
 python3 run.py --metadata ${1} --dataset ${2} --processor ${3}
 ls hists/${3}/${2}.futures
 cp hists/${3}/${2}.futures ${_CONDOR_SCRATCH_DIR}/${3}_${2}.futures
