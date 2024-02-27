1a47ad003dcf57cc37e395a9f8a4e8901d1cad75
Matteo Cremonesi <matteoc@fnal.gov>
2024-02-26 21:54:38 -0500

    Update list.py

--- analysis/macros/list.py
+++ analysis/macros/list.py
@@ -1,41 +1,81 @@
 #!/usr/bin/env python
 import os
+from data.process import *
 from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option('-y', '--year', help='year', dest='year')
+parser.add_option('-d', '--dataset', help='dataset', dest='dataset')
+parser.add_option('-m', '--metadata', help='metadata', dest='metadata')
+parser.add_option('-p', '--pack', help='pack', dest='pack')
+parser.add_option('-s', '--special', help='special', dest='special')
 (options, args) = parser.parse_args()
 
 globalredirect = "root://xrootd-cms.infn.it/"
 
 campaigns ={}
-campaigns['2016'] = 'RunIISummer16MiniAODv3'
-campaigns['2017'] = 'RunIIFall17MiniAODv2'
-campaigns['2018'] = 'RunIIAutumn18NanoAODv6'
+campaigns['2016pre'] = '*UL*16*JMENano'
+campaigns['2016post'] = '*UL*16*JMENano'
+campaigns['2017'] = '*UL*17*JMENano'
+campaigns['2018'] = '*UL*18*JMENano'
 
-slist = ['DarkHiggs_MonoHs_LO_TuneCP5_13TeV-madgraph-pythia8']
+def split(arr, size):
+     arrs = []
+     while len(arr) > size:
+         pice = arr[:size]
+         arrs.append(pice)
+         arr   = arr[size:]
+     arrs.append(arr)
+     return arrs
+
+xsections={}
+for k,v in processes.items():
+     if v[1]=='MC':
+          if not isinstance(k, str):
+               if options.year!=str(k[1]): continue
+               xsections[k[0]] = v[2]
+          else: 
+               xsections[k] = v[2]
+     else:
+          xsections[k] = -1
+
+datadef = {}
 datasets = []
-for sample in slist:
-    query="dasgoclient --query=\"dataset dataset=/"+sample+"*/"+campaigns[options.year]+"*/NANOAODSIM\""
+for dataset in xsections.keys():
+    if options.dataset:
+        if not any(_dataset in dataset for _dataset in options.dataset.split(',')): continue
+    query="dasgoclient --query=\"file dataset=/"+dataset+"*/"+campaigns[options.year]+"*/NANOAODSIM\""
     print(query)
-    os.system(query+" >"+sample+".txt")
-    datasets.append(sample+".txt")
-
-filenames = []
-for dname in datasets:                                                                                                                                                                 
-    with open(dname) as infile:
-        for line in infile:
-            dataset = line.strip()
-            query="dasgoclient --query=\"file dataset="+dataset+"\""
-            print(query)
-            os.system(query+" > "+dataset.split('/')[1]+"____"+dataset.split('/')[2]+".txt")
-            filenames.append(dataset.split('/')[1]+"____"+dataset.split('/')[2]+".txt")
-    os.system('rm '+dname)
-
-for sample in slist:
-    with open('metadata/'+sample+'.txt', 'w') as outfile:
-        for fname in filenames:
-            with open(fname) as infile:
-                for line in infile:
-                    outfile.write(globalredirect+line)
-            os.system('rm '+fname)
+    urllist = datasets.append(os.popen(query).read().split("\n"))
+    for url in urllist[:]:
+        if options.year not in str(url):
+            urllist.remove(url)
+            continue
+        if '.root' not in url: 
+            urllist.remove(url)
+            continue
+        urllist[urllist.index(url)]=globalredirect+url
+    print('list lenght:',len(urllist))
+    if options.special:
+         for special in options.special.split(','):
+              sdataset, spack = special.split(':')
+              if sdataset in dataset:
+                   print('Packing',spack,'files for dataset',dataset)
+                   urllists = split(urllist, int(spack))
+              else:
+                   print('Packing',int(options.pack),'files for dataset',dataset)
+                   urllists = split(urllist, int(options.pack))
+    else:
+         print('Packing',int(options.pack),'files for dataset',dataset)
+         urllists = split(urllist, int(options.pack))
+    print(len(urllists))
+    if urllist:
+        for i in range(0,len(urllists)) :
+             datadef[dataset+"____"+str(i)+"_"] = {
+                  'files': urllists[i],
+                  'xs': xs,
+                  }
+        
+folder = "metadata/"+options.metadata+".json"
+with open(folder, "w") as fout:
+    json.dump(datadef, fout, indent=4)
