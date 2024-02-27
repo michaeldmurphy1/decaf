2bbe376b30f00fec4fc49be725b34edc6187755e
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2024-02-27 11:22:36 +0900

    fixing remove corrupted

--- analysis/macros/remove_corrupted.py
+++ analysis/macros/remove_corrupted.py
@@ -6,6 +6,8 @@ import sys
 
 parser = OptionParser()
 parser.add_option('-m', '--metadata', help='metadata', dest='metadata', default='2018')
+parser.add_option('-s', '--save', help='save', dest='save', default=None)
+parser.add_option('-l', '--lists', help='lists', dest='lists', default=None)
 (options, args) = parser.parse_args()
 
 list_corrupted = []
@@ -17,9 +19,15 @@ for output in os.popen('grep \'rror\' logs/condor/run/err/*').read().split("\n")
         rootfile = line.strip().split('root://')[1]
         if 'root://'+rootfile in list_corrupted: continue
         list_corrupted.append('root://'+rootfile)
+if options.save:
+    with open('/data/'+options.save+'_list_corrupted.pkl', 'wb') as fp:
+        pickle.dump(list_corrupted, fp)    
+if options.lists:
+    with open ('/data/'+options.lists+'_list_corrupted.pkl', 'rb') as fp:
+        list_corrupted = pickle.load(fp)
 if len(list_corrupted)<1:
     sys.exit('No corrupted files')
-    
+
 dictionary={}
 with open(options.metadata) as fin:
     dictionary.update(json.load(fin))
